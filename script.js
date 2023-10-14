d019033e3e3620609246a88cc3f78618e58bcdaa
saikiranp369 <147926284+saikiranp369@users.noreply.github.com>
2023-10-14 22:26:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const content = document.getElementById("content");
-    const addTextBtn = document.getElementById("addText");
-    const addImageBtn = document.getElementById("addImage");
-    const addVideoBtn = document.getElementById("addVideo");
-    addTextBtn.addEventListener("click", () => {
-        const text = prompt("Enter your text:");
-        if (text) {
-            const textElement = document.createElement("p");
-            textElement.textContent = text;
-            content.appendChild(textElement);
-        }
-    });
-    addImageBtn.addEventListener("click", () => {
-        const imageUrl = prompt("Enter the image URL:");
-        if (imageUrl) {
-            const imageElement = document.createElement("img");
-            imageElement.src = imageUrl;
-            content.appendChild(imageElement);
-        }
-    });
-    addVideoBtn.addEventListener("click", () => {
-        const videoUrl = prompt("Enter the video URL (YouTube or Vimeo):");
-        if (videoUrl) {
-            const videoElement = document.createElement("iframe");
-            videoElement.src = videoUrl;
-            videoElement.allowFullscreen = true;
-            content.appendChild(videoElement);
-        }
-    });
-});
+function addText() {
+    const textInput = document.getElementById('textInput').value;
+    const displayArea = document.getElementById('displayArea');
+    const textElement = document.createElement('p');
+    textElement.textContent = textInput;
+    displayArea.appendChild(textElement);
+}
+
+function addImage() {
+    const imageInput = document.getElementById('imageInput');
+    const displayArea = document.getElementById('displayArea');
+
+    if (imageInput.files && imageInput.files[0]) {
+        const imgElement = document.createElement('img');
+        imgElement.src = URL.createObjectURL(imageInput.files[0]);
+        displayArea.appendChild(imgElement);
+    }
+}
+
+function addVideo() {
+    const videoInput = document.getElementById('videoInput').value;
+    const displayArea = document.getElementById('displayArea');
+    const videoElement = document.createElement('video');
+    videoElement.src = videoInput;
+    videoElement.controls = true;
+    displayArea.appendChild(videoElement);
+}
